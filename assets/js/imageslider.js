7a0c7299dd25dda5fbd9386b1943cd3bd1cb99aa
agimsherifi <agimsherifi1993@gmail.com>
2023-08-13 00:14:18 +0200

    Final Edition

--- assets/js/imageslider.js
+++ assets/js/imageslider.js
@@ -1,11 +1,11 @@
-var images = ['/assets/image/rockyellow.jpg', '/assets/image/rockyellow2.jpg', '/assets/image/rockyellow3.jpg',
-              '/assets/image/blerjetphoto/skeleton.png', '/assets/image/blerjetphoto/skeleton2.png', '/assets/image/blerjetphoto/skeleton3.png',
-              '/assets/image/blerjetphoto/deep.png', '/assets/image/blerjetphoto/deep2.png', '/assets/image/blerjetphoto/deep3.png',
-              '/assets/image/blerjetphoto/primeline.png', '/assets/image/blerjetphoto/primeline2.png', '/assets/image/blerjetphoto/primeline3.png',
-              '/assets/image/blerjetphoto/tempomaster.png', '/assets/image/blerjetphoto/tempomaster2.png', '/assets/image/blerjetphoto/tempomaster3.png',
-              '/assets/image/blerjetphoto/valias.png', '/assets/image/blerjetphoto/valias2.png', '/assets/image/blerjetphoto/valias3.png',
-              '/assets/image/blerjetphoto/venus.png', '/assets/image/blerjetphoto/venus2.png', '/assets/image/blerjetphoto/venus3.png',
-              '/assets/image/blerjetphoto/searock.png', '/assets/image/blerjetphoto/searock2.png', '/assets/image/blerjetphoto/searock3.png',];
+var images = ['assets/image/rockyellow.jpg', 'assets/image/rockyellow2.jpg', 'assets/image/rockyellow3.jpg',
+              'assets/image/blerjetphoto/skeleton.png', 'assets/image/blerjetphoto/skeleton2.png', 'assets/image/blerjetphoto/skeleton3.png',
+              'assets/image/blerjetphoto/deep.png', 'assets/image/blerjetphoto/deep2.png', 'assets/image/blerjetphoto/deep3.png',
+              'assets/image/blerjetphoto/primeline.png', 'assets/image/blerjetphoto/primeline2.png', 'assets/image/blerjetphoto/primeline3.png',
+              'assets/image/blerjetphoto/tempomaster.png', 'assets/image/blerjetphoto/tempomaster2.png', 'assets/image/blerjetphoto/tempomaster3.png',
+              'assets/image/blerjetphoto/valias.png', 'assets/image/blerjetphoto/valias2.png', 'assets/image/blerjetphoto/valias3.png',
+              'assets/image/blerjetphoto/venus.png', 'assets/image/blerjetphoto/venus2.png', 'assets/image/blerjetphoto/venus3.png',
+              'assets/image/blerjetphoto/searock.png', 'assets/image/blerjetphoto/searock2.png', 'assets/image/blerjetphoto/searock3.png',];
         var currentImageIndex = 0;
     
         function changeImage(index) {
